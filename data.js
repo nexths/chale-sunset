44b99238053403627ad9ea398d3c2661c71a54a9
nexths <henriquenik@gmail.com>
2026-09-04 14:16:44 -0300

    Primeiro commit do Chale Sunset

---
+++ data.js
@@ -0,0 +1,378 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fachada",
+      "name": "Fachada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.12364711214489432,
+        "pitch": 0.04338620234390689,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6016557291218916,
+          "pitch": 0.39338470317003527,
+          "rotation": 0,
+          "target": "1-rea-social",
+          "targetViewParameters": {
+            "yaw": 0.04216976352066659
+          }
+        },
+        {
+          "yaw": 1.1575081144533446,
+          "pitch": 0.25798928946654982,
+          "rotation": 0,
+          "target": "5-trilha-de-chegada",
+          "targetViewParameters": {
+          "yaw": -2.8992090496904837
+          }
+        },
+        {
+          "yaw": -0.28329931458426856,
+          "pitch": 0.1777623103103467,
+          "rotation": 0,
+          "target": "7-hidro-externa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-rea-social",
+      "name": "Área Social",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.5177926641249151,
+        "pitch": 0.050306512870687214,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.587007926258587,
+          "pitch": 0.456766788995578,
+          "rotation": 0,
+          "target": "6-bathroom"
+        },
+        {
+          "yaw": 3.12130999564282,
+          "pitch": 0.4024891376552725,
+          "rotation": 0,
+          "target": "0-fachada",
+          "targetViewParameters": {
+            "yaw": -2.5137820977665104
+          }
+        },
+        {
+          "yaw": -0.9373543030502915,
+          "pitch": -0.013707384770945907,
+          "rotation": 6.283185307179586,
+          "target": "2-mezanino"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-mezanino",
+      "name": "Mezanino",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.054666261738749,
+          "pitch": 0.45949171186361966,
+          "rotation": 3.9269908169872414,
+          "target": "1-rea-social",
+          "targetViewParameters": {
+            "yaw": 2.8520883145253517
+          }
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.06572187234743154,
+        "pitch": -0.007380357407068772,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03577957984669112,
+          "pitch": 0.2612630699689973,
+          "rotation": 0,
+          "target": "4-caminho-do-chal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-caminho-do-chal",
+      "name": "Caminho do Chalé",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0995698772843596,
+          "pitch": 0.2677992916129881,
+          "rotation": 0,
+          "target": "3-entrada",
+          "targetViewParameters": {
+            "yaw": -2.9506072321022625
+          }
+        },
+        {
+          "yaw": 0.0739421702022014,
+          "pitch": 0.35044203651055916,
+          "rotation": 0,
+          "target": "5-trilha-de-chegada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-trilha-de-chegada",
+      "name": "Trilha de Chegada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 1.6176260953217474,
+        "pitch": 0.06586186910649516,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5106917061225609,
+          "pitch": 0.1828733890175105,
+          "rotation": 0,
+          "target": "4-caminho-do-chal",
+          "targetViewParameters": {
+          "yaw": 3.2261689751206018
+          }
+        },
+        {
+          "yaw": 0.6543463084637153,
+          "pitch": 0.2537230753703881,
+          "rotation": 0,
+          "target": "0-fachada",
+          "targetViewParameters": {
+          "yaw": -2.0594612392567857
+          }
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.20721562995708,
+        "pitch": 0.02801334491561036,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7600956696594654,
+          "pitch": 0.36288611585165675,
+          "rotation": 0,
+          "target": "1-rea-social",
+          "targetViewParameters": {
+          "yaw": 2.8520883145253517
+          }
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-hidro-externa",
+      "name": "Hidro Externa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 2.507112112297733,
+        "pitch": -0.04291308106234659,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6091607666016792,
+          "pitch": 0.30105127308193005,
+          "rotation": 0,
+          "target": "0-fachada",
+          "targetViewParameters": {
+          "yaw": 2.9762320993826323
+          }
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Chalá Sunset",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
